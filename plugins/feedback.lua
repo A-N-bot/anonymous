1906132d884200ba60d1bd12292bacccaf918c7a
Nima <anonymou3team@gmail.com>
2016-04-20 19:59:33 +0430

    Create feedback.lua

---
+++ plugins/feedback.lua
@@ -0,0 +1,53 @@
+do
+
+ function run(msg, matches)
+
+ 
+
+  local fuse = 'New FeedBack\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username .. '\n\nThe Pm:\n' .. matches[1]  
+
+ local fuses = '!printf user#id' .. msg.from.id
+
+ 
+
+ 
+
+   local text = matches[1]
+
+   local chat = "chat#id"..90312082 
+
+   --like : local chat = "chat#id"..12345678
+
+   
+
+  local sends = send_msg(chat, fuse, ok_cb, false)
+
+  return '📝پـــــــيــــام شــمـــا ارســــال شـــــد❕'
+
+ 
+
+ end
+
+ end
+
+ return {
+
+  
+
+  description = "Feedback",
+
+ 
+
+  usage = "!feedback message",
+
+  patterns = {
+
+  "^[!/][Ff]eedback (.*)$"
+
+ 
+
+  },
+
+  run = run
+
+ }
