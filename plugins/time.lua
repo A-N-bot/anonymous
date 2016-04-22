5a892f74cbe0db232704ef3aac82364ecb1c8138
Nima <anonymou3team@gmail.com>
2016-04-22 16:35:16 -0700

    Added files via upload

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,3 +1,13 @@
+--[[
+#
+#    Time And Date
+#
+#    @Dragon_born
+#	@GPMod
+#
+#
+]]
+
 function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
@@ -9,3 +19,5 @@ return {
   patterns = {"^[/!]([Tt][iI][Mm][Ee])$"}, 
 run = run 
 }
+
+
