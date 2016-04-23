8853fed1b710a42ba7147cd80d9e2ced8f85de33
anonymou3tea <anonymou3team@gmail.com>
2016-04-22 18:59:12 -0700

    Update salam-m.lua

--- plugins/salam-m.lua
+++ plugins/salam-m.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'سلام عزیزم ،خوش اومدی🌹'
+local text = '😃سلام مدیر..عزییزم خوش اومدی'
 if matches[1] == 'salam' or 'سلام' then
     if is_momod(msg) then
 reply_msg(reply_id, text, ok_cb, false)
