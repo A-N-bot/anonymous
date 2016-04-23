fd2f90a3998416f22c962b79d419754126a59b06
anonymou3tea <anonymou3team@gmail.com>
2016-04-22 19:03:10 -0700

    Update bye.s.lua

--- plugins/bye.s.lua
+++ plugins/bye.s.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'خدافظ باباجونم ^_^'
+local text = '😖خدافظ باباجووونم...دلم واست تنگ میشه'
 if matches[1] == 'بای' or 'خدانگهدار' then
     if is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
