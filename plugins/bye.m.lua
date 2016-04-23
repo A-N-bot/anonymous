f7baa228eb82ce4399a3d5805fe583ff84947608
anonymou3tea <anonymou3team@gmail.com>
2016-04-22 18:52:16 -0700

    Added files via upload

---
+++ plugins/bye.m.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+local reply_id = msg['id']
+local text = 'خدافظ باباجونم ^_^'
+if matches[1] == 'بای' or 'خدانگهدار' then
+    if is_momod(msg) then
+reply_msg(reply_id, text, ok_cb, false)
+end
+end 
+end
+return {
+patterns = {
+    "بای",
+    "خدانگهدار"
+},
+run = run
+}
+
+end
+
+
