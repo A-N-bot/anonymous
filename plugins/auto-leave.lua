00658e7a9ab91e1bf83c1899d8a46173d8b62a3b
anonymou3tea <anonymou3team@gmail.com>
2016-04-24 03:00:08 +0430

    Update auto-leave.lua

--- plugins/auto-leave.lua
+++ plugins/auto-leave.lua
@@ -1,34 +1,21 @@
---[[
-Kicking ourself (bot) from unmanaged groups.
-
-When someone invited this bot to a group, the bot then will chek if the group is in its moderations (moderation.json). If not, the bot will exit immediately by kicking itself out of that group.
-
-No switch available. You need to turn it on or off using !plugins command.
-
-Testing needed.
---]]
-
--- suppress '*** lua: attempt to call a nil value' warning
-local function callback(extra, success, result)
-  vardump(success)
-  vardump(result)
-end
-
-local function run(msg)
-  if msg.service and msg.action.type == 'chat_add_user' then
-    local data = load_data(_config.moderation.data)
-    if not data[tostring(msg.to.id)] then
-      print "This is not our group. Leaving..."
-      chat_del_user('chat#id'..msg.to.id, 'user#id'..our_id, callback, false)
+do
+-- Will leave the group if be added
+local function run(msg, matches)
+local bot_id = our_id -- your bot id
+   -- like local bot_id = 1234567
+    if matches[1] == 'leave' and is_admin(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+       send_large_msg("chat#id"..msg.to.id, '❕بدلیل بدون اجازه ادد کردن من شما بلاک میشوید و با تکرار دوباره این عمل از همه گروه های من حذف میشوید خداحفــــــــظ❕', ok_cb, false)
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     end
-  end
 end
-
+ 
 return {
-  description = "Kicking ourself (bot) from unmanaged groups.",
-  usage = "No switch available. Turn it on or off using !plugins command.",
   patterns = {
-    "^!!tgservice (.+)$"
+    "^[!/](leave)$",
+    "^!!tgservice (.+)$",
   },
   run = run
 }
+end
